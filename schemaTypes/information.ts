dd02cb3da7fa43c14244c3789786c88078fdd695
Hoàng Minh Thiên <hoangminhthien05022009@gmail.com>
2025-05-12 15:14:25 +0700

    feat: updated schema to match the current requirements

--- schemaTypes/information.ts
+++ schemaTypes/information.ts
@@ -11,19 +11,87 @@ export const information = defineType({
       validation: (rule) => rule.required(),
     }),
     defineField({
+      name: 'titleAlignment',
+      type: 'string',
+      options: {
+        list: [
+          {title: 'Left', value: 'left'},
+          {title: 'Center', value: 'center'},
+          {title: 'Right', value: 'right'},
+        ],
+      },
+      initialValue: 'center',
+    }),
+    defineField({
+      name: 'titleVerticalAlignment',
+      type: 'string',
+      options: {
+        list: [
+          {title: 'Top', value: 'top'},
+          {title: 'Middle', value: 'middle'},
+          {title: 'Bottom', value: 'bottom'},
+        ],
+      },
+      initialValue: 'middle',
+    }),
+    defineField({
+      name: 'subtitle',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'primaryColor',
+      type: 'string',
+      validation: (rule) => rule.required().regex(/^#[0-9a-fA-F]{6}$/),
+    }),
+    defineField({
       name: 'slug',
       type: 'slug',
       options: {source: 'title'},
       validation: (rule) => rule.required(),
     }),
     defineField({
-      name: 'image',
+      name: 'mainImage',
       type: 'image',
+      validation: (rule) => rule.required(),
     }),
     defineField({
-      name: 'body',
+      name: 'introduction',
+      type: 'text',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'UNESCORecognizationTitle',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'UNESCORecognizationYear',
+      type: 'number',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'costumeImage',
+      type: 'image',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'firstTextSection',
       type: 'array',
       of: [{type: 'block'}],
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'youtubeLinks',
+      type: 'array',
+      of: [{type: 'string'}],
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'secondTextSection',
+      type: 'array',
+      of: [{type: 'block'}],
+      validation: (rule) => rule.required(),
     }),
   ],
 })
